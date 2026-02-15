5abf56ad8bfc064df52fe677a598e2b0b919cf24
hanna <me@hanna.lol>
2026-02-15 19:27:26 +0000

    feat: add static binary builds and GitHub release workflow
    - Update flake.nix with volt-static package using musl for Linux
      (nix build .#volt-static produces a fully static binary)
    - Add .github/workflows/release.yml that builds static Linux binaries
      and regular macOS binaries for all 4 targets (x86_64/aarch64 ×
      linux/darwin), uploads artifacts, and creates GitHub releases on
      version tags

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
       perSystem = {
         system,
         pkgs,
+        lib,
         ...
       }: let
         fenixPkgs = inputs.fenix.packages.${system};
@@ -41,11 +42,69 @@
           // {
             inherit cargoArtifacts;
           });
+
+        # Static binary support (Linux only, using musl)
+        muslTarget =
+          if system == "x86_64-linux"
+          then "x86_64-unknown-linux-musl"
+          else if system == "aarch64-linux"
+          then "aarch64-unknown-linux-musl"
+          else null;
+
+        staticToolchain =
+          if muslTarget != null
+          then
+            fenixPkgs.combine [
+              (fenixPkgs.stable.withComponents [
+                "cargo"
+                "rustc"
+                "rust-src"
+              ])
+              fenixPkgs.targets.${muslTarget}.stable.rust-std
+            ]
+          else null;
+
+        staticCraneLib =
+          if staticToolchain != null
+          then (inputs.crane.mkLib pkgs).overrideToolchain staticToolchain
+          else null;
+
+        staticCommonArgs =
+          if muslTarget != null
+          then {
+            src = craneLib.cleanCargoSource ./.;
+            strictDeps = true;
+            CARGO_BUILD_TARGET = muslTarget;
+            CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+            HOST_CC = "${pkgs.stdenv.cc.nativePrefix}cc";
+            TARGET_CC = "${pkgs.pkgsStatic.stdenv.cc}/bin/${pkgs.pkgsStatic.stdenv.cc.targetPrefix}cc";
+            "CARGO_TARGET_${lib.toUpper (builtins.replaceStrings ["-"] ["_"] muslTarget)}_LINKER" =
+              "${pkgs.pkgsStatic.stdenv.cc}/bin/${pkgs.pkgsStatic.stdenv.cc.targetPrefix}cc";
+          }
+          else {};
+
+        staticCargoArtifacts =
+          if staticCraneLib != null
+          then staticCraneLib.buildDepsOnly staticCommonArgs
+          else null;
+
+        volt-static =
+          if staticCraneLib != null
+          then
+            staticCraneLib.buildPackage (staticCommonArgs
+              // {
+                cargoArtifacts = staticCargoArtifacts;
+              })
+          else null;
       in {
-        packages = {
-          default = volt;
-          volt = volt;
-        };
+        packages =
+          {
+            default = volt;
+            volt = volt;
+          }
+          // lib.optionalAttrs (volt-static != null) {
+            volt-static = volt-static;
+          };
 
         checks = {
           inherit volt;
